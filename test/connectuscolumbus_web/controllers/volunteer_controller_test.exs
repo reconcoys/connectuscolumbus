1a0e45346e0b75d73441ca21b6068b6459d6d2be
Connor Sweeney <cs293407@gmail.com>
2020-04-13 13:39:03 -0400

    testing failing pr build

--- test/connectuscolumbus_web/controllers/volunteer_controller_test.exs
+++ test/connectuscolumbus_web/controllers/volunteer_controller_test.exs
@@ -15,7 +15,7 @@ defmodule ConnectuscolumbusWeb.VolunteerControllerTest do
   describe "index" do
     test "lists all volunteers", %{conn: conn} do
       conn = get(conn, Routes.volunteer_path(conn, :index))
-      assert html_response(conn, 200) =~ "Listing Volunteers"
+      assert html_response(conn, 200) =~ "1Listing Volunteers"
     end
   end
 
