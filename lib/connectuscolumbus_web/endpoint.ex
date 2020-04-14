2a5d89fa6ee1dc79e9e7d90d21a0060817327527
evanawebb <webbevana@gmail.com>
2020-04-13 20:53:05 -0400

    add authentication routes using pow

--- lib/connectuscolumbus_web/endpoint.ex
+++ lib/connectuscolumbus_web/endpoint.ex
@@ -10,38 +10,42 @@ defmodule ConnectuscolumbusWeb.Endpoint do
     signing_salt: "RCEAbZOF"
   ]
 
-  socket "/socket", ConnectuscolumbusWeb.UserSocket,
+  socket("/socket", ConnectuscolumbusWeb.UserSocket,
     websocket: true,
     longpoll: false
+  )
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug Plug.Static,
+  plug(Plug.Static,
     at: "/",
     from: :connectuscolumbus,
     gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
+  )
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-    plug Phoenix.LiveReloader
-    plug Phoenix.CodeReloader
+    socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
+    plug(Phoenix.LiveReloader)
+    plug(Phoenix.CodeReloader)
   end
 
-  plug Plug.RequestId
-  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+  plug(Plug.RequestId)
+  plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
 
-  plug Plug.Parsers,
+  plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
+  )
 
-  plug Plug.MethodOverride
-  plug Plug.Head
-  plug Plug.Session, @session_options
-  plug ConnectuscolumbusWeb.Router
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
+  plug(Plug.Session, @session_options)
+  plug(Pow.Plug.Session, otp_app: :connectuscolumbus)
+  plug(ConnectuscolumbusWeb.Router)
 end
--- lib/connectuscolumbus_web/router.ex
+++ lib/connectuscolumbus_web/router.ex
@@ -1,24 +1,36 @@
 defmodule ConnectuscolumbusWeb.Router do
   use ConnectuscolumbusWeb, :router
+  use Pow.Phoenix.Router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
+  end
+
+  pipeline :protected do
+    plug(Pow.Plug.RequireAuthenticated,
+      error_handler: Pow.Phoenix.PlugErrorHandler
+    )
+  end
+
+  scope "/" do
+    pipe_through(:browser)
+    pow_routes()
   end
 
   scope "/", ConnectuscolumbusWeb do
-    pipe_through :browser
+    pipe_through([:browser, :protected])
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
 
-    resources "/volunteers", VolunteerController
+    resources("/volunteers", VolunteerController)
   end
 
   # Other scopes may use custom stacks.
